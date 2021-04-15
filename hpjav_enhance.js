37fc3f103d212cfd53934a8f1bd1b6a5b8c88ec5
sancunguangyin <upc_tianxin@foxmail.com>
2021-04-15 17:41:39 +0800

    adaptive tampermonkey

--- hpjav_enhance.js
+++ hpjav_enhance.js
@@ -1,17 +1,34 @@
 // ==UserScript==
-// @name         HPJAV增强
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  HPJAV增强
+// @name         HPJAV Enhance
+// @namespace    https://github.com/isTianXin/GMScript/
+// @version      1.0
+// @description  HPJAV Enhance
 // @author       You
 // @match        *://hpjav.tv/*
 // @grant        GM_openInTab
+// @grant        GM_info
 // @run-at       document-end
 // ==/UserScript==
 
 'use strict';
 
 const OPEN_ALL_TORRENT_LINKS_BUTTON = "openAllLinks";
+
+const SCRIPT_HANDLER_TAMPERMONKEY = 'tampermonkey';
+
+let isTampermonkey = () => {
+    return (GM_info || { scriptHandler: '' }).scriptHandler.toLowerCase() === SCRIPT_HANDLER_TAMPERMONKEY;
+}
+
+let getAdaptivedDocument = () => {
+    //Tampermonkey will get first iframe, may be a bug
+    if (isTampermonkey()) {
+        return window.parent.document;
+    }
+    return window.document;
+}
+
+let adaptivedDocument = getAdaptivedDocument();
 
 let getTorrentFunkLink = (key) => {
     return `https://www.torrentfunk.com/all/torrents/${key}.html`;
@@ -31,14 +48,14 @@ let replaceDateTextWithTorrentFunkLink = (card) => {
     card.insertAdjacentHTML("beforeend", jumpLink);
 }
 let insertTorrentFunkLink = () => {
-    let cards = Array.from(document.querySelectorAll("#ajax-fax > div > div.entry-title"));
+    let cards = Array.from(adaptivedDocument.querySelectorAll("#ajax-fax > div > div.entry-title"));
     if (!cards) {
         return;
     }
     cards.forEach(replaceDateTextWithTorrentFunkLink);
 }
 let openAllTorrentFunkLinks = () => {
-    Array.from(document.querySelectorAll("#ajax-fax > div > div.entry-title > a:nth-child(1)"))
+    Array.from(adaptivedDocument.querySelectorAll("#ajax-fax > div > div.entry-title > a:nth-child(1)"))
         .map(item => item.innerText.split(" ")[0])
         .map(getTorrentFunkLink)
         .forEach(
@@ -53,17 +70,15 @@ let openAllTorrentFunkLinks = () => {
         );
 }
 let insertOpenAllButton = () => {
-    let buttons = document.querySelector("body > section > div > div > div.category-count > span");
+    let buttons = adaptivedDocument.querySelector("body > section > div > div > div.category-count > span");
     let btn = `<a class="btn btn-sm btn-default" id="${OPEN_ALL_TORRENT_LINKS_BUTTON}" rel="nofollow">Open All Torrent Links</a>`;
     buttons.insertAdjacentHTML("afterbegin", btn);
 }
 let start = () => {
-    //插入按钮
     insertTorrentFunkLink();
     insertOpenAllButton();
 }
 /**
- * 延时执行脚本
  * @param {integer} interval
  */
 let startWithInterval = (interval) => {
@@ -72,8 +87,8 @@ let startWithInterval = (interval) => {
 
 startWithInterval(1000);
 
-document.onclick = e => {
-    //搜索全部区县 onclick
+adaptivedDocument.onclick = e => {
+    //openAll onclick
     if (e.target.id === OPEN_ALL_TORRENT_LINKS_BUTTON) {
         let clicked = false;
         if (!clicked) {
