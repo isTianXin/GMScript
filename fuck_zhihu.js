0ed94976303886e665dc78ff18cf28c4ef54f314
sancunguangyin <upc_tianxin@foxmail.com>
2021-04-16 17:14:28 +0800

    add fuck zhihu

---
+++ fuck_zhihu.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Fuck ZhiHu
+// @namespace    https://github.com/isTianXin/GMScript/
+// @version      1.0
+// @description  去除知乎中的令人不爽的内容
+// @author       sancunguangyin
+// @match        https://www.zhihu.com/question/*
+// @match        https://www.zhihu.com
+// @run-at       document-end
+// ==/UserScript==
+
+'use strict';
+
+/**
+ * 去除答案列表会员专享
+ */
+let removeVipContent = () => {
+    Array.from(document.querySelectorAll("div.ContentItem.AnswerItem > div.KfeCollection-AnswerTopCard-Container")).forEach((item) => {
+        console.log(item.parentElement);
+        item.parentElement.parentElement.remove();
+    });
+};
+
+/**
+ * 去除推荐页视频
+ */
+let removeRecommendVideo = () => {
+    Array.from(document.querySelectorAll("div.Feed > div.ContentItem.ZVideoItem")).forEach((item) => {
+        console.log(item.parentElement);
+        item.parentElement.parentElement.remove();
+    });
+};
+
+let start = () => {
+    removeRecommendVideo();
+    removeVipContent();
+};
+
+start();
+
+window.onscroll = () => {
+    start();
+};
