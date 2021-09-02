96304658af336935d2aea4acfa876a97a9859093
tianxin <52024071+isTianXin@users.noreply.github.com>
2021-09-02 17:30:17 +0800

    Update fuck_zhihu.js

--- fuck_zhihu.js
+++ fuck_zhihu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fuck ZhiHu
 // @namespace    https://github.com/isTianXin/GMScript/
-// @version      1.0
+// @version      1.1
 // @description  去除知乎中的令人不爽的内容
 // @author       sancunguangyin
 // @match        https://www.zhihu.com/question/*
@@ -29,8 +29,19 @@ let removeRecommendVideo = () => {
     });
 };
 
+/**
+ * 去除推荐页混合视频的回答
+ */
+let removeRecommendMixedVideo = () => {
+    Array.from(document.querySelectorAll("div.RichContent.is-collapsed > div.RichContent-cover > svg")).forEach((item) => {
+        console.log(item);
+        item.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+    });
+}
+
 let start = () => {
     removeRecommendVideo();
+    removeRecommendMixedVideo();
     removeVipContent();
 };
 
